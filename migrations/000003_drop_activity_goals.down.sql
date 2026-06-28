61f73587102da194bd02181b3b438e738db2b546
Night-Swan <naren42651@gmail.com>
2026-06-28 15:49:59 +0200

    feat: drop activity_level and goals columns from profile schema

---
+++ migrations/000003_drop_activity_goals.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE profile ADD COLUMN activity_level TEXT;
+ALTER TABLE profile ADD COLUMN goals TEXT;
---
+++ migrations/000003_drop_activity_goals.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE profile DROP COLUMN activity_level;
+ALTER TABLE profile DROP COLUMN goals;
